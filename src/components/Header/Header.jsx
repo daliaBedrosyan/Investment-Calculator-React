4ca44f9704e61d75065d0f2d6ef59f5cc7aedac3
dalia9999 <daliabedrosyan47@gmail.com>
2025-02-04 18:26:29 +0200

    Changed: Header and added UserInput component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,8 @@
 export default function Header() {
     return (
-        <>
-            <div className="d-flex justify-content-center">
-                <img src="./public/budget.png" alt="..." className="mt-5 col col-2" style={{ width: '100px' }} />
-            </div>
+        <header id="header">
+                <img src="./public/budget.png" alt="Logo showing money" style={{ width:'90px' }}/>
             <h1 id="header" className="text-light fw-bold">Investment Calculator</h1>
-        </>
+        </header>
     );
 }
